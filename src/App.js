123403b54df205a9b18c4bd1db71f5755ed2e7a2
arifuxd <arifofficial00@gmail.com>
2021-08-16 18:55:11 +0600

    user style done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Message from './Message';
 
 function App() {
+  const [text, setText] = useState('')
+  const [messages, setMessegas] = useState([
+    {username : 'Ariful Islam', text : 'Hello Brother'},
+    {username : 'Akash Islam', text : 'Are You okay?'}
+  ])
+  const [username, setUsername] = useState('')
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    setMessegas([...messages, {username,text}])
+    setText('')
+  }
+
+  useEffect(()=>{
+    setUsername(prompt('Whats Your Name'))
+  }, [])
+
+
+  console.log(messages)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Messenger App</h1>
+     <h2>Welcome {username}</h2>
+    <form >
+      <FormControl>
+        <InputLabel>Enter a message</InputLabel>
+      <Input value={text} onChange={(e)=> setText(e.target.value)} type="text" />
+      </FormControl>
+      <Button disabled={!text} onClick={sendMessage} color="secondary" variant="contained" type="submit">Send Message</Button>
+
+    </form>
+
+    {messages.map(message => (
+      <Message username={username} message={message}/>
+    ))}
+
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,25 @@
+import { Card, CardContent, Typography } from '@material-ui/core'
+import React from 'react'
+import './Message.css'
+const Message = ({message, username}) => {
+    const isUser = username === message.username;
+    console.log(isUser)
+    return (
+        <div className={`message ${isUser && 'message_user'}`}>
+        <Card className={isUser ? "message_userCard" : "message_guestCard"} >
+            <CardContent>
+                <Typography
+                color="white"
+                variant="h5"
+                component = "h2"
+                >
+                  {message.username}  {message.text}  
+                </Typography>
+            </CardContent>
+        </Card>
+      
+        </div>
+    )   
+}
+
+export default Message
